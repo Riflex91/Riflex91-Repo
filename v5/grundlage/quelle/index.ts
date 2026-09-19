e54d1869cb7cd069bdab91483650751a3708fdb8
Riflex91 <hansijuergenlul@gmail.com>
2026-09-20 00:09:14 +0200

    grundlage(v5): exportiere R3 no-write Grundvertraege

---
+++ v5/grundlage/quelle/index.ts
@@ -0,0 +1,7 @@
+export * from "./kern/ausfuehrungs-freigabe.js";
+export * from "./kern/faehigkeits-schalter.js";
+export * from "./host/host-grenze.js";
+export * from "./nachrichten/nachrichten-umschlag.js";
+export * from "./persistenz/begrenzter-asynchroner-schreiber.js";
+export * from "./persistenz/speicher-port.js";
+export * from "./grundlage/leere-v5-grundlage.js";
