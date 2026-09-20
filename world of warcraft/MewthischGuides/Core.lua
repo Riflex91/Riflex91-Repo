e0accd1c320d546deb94279514d96585bd9dfcb9
Riflex91 <hansijuergenlul@gmail.com>
2026-09-20 15:14:55 +0200

    WoW: set Mewthisch Guides v0.5.1 runtime version

--- world of warcraft/MewthischGuides/Core.lua
+++ world of warcraft/MewthischGuides/Core.lua
@@ -2,7 +2,7 @@ local addonName, MG = ...
 _G.MewthischGuides = MG
 _G.ForeverGuide = MG
 
-MG.VERSION = "0.5.0"
+MG.VERSION = "0.5.1"
 MG.INTERFACE = 16001
 MG.NAME = "Mewthisch Guides"
 MG.heartbeatTicker = nil
