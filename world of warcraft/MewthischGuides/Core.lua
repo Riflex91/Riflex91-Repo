2caa9b43226413f8718affbb8fe9f42e7caacb1d
Riflex91 <hansijuergenlul@gmail.com>
2026-09-20 18:33:19 +0200

    WoW: bump runtime-fix build to v0.11.1

--- world of warcraft/MewthischGuides/Core.lua
+++ world of warcraft/MewthischGuides/Core.lua
@@ -2,7 +2,7 @@ local addonName, MG = ...
 _G.MewthischGuides = MG
 _G.ForeverGuide = MG
 
-MG.VERSION = "0.11.0"
+MG.VERSION = "0.11.1"
 MG.INTERFACE = 16001
 MG.NAME = "Mewthisch Guides"
 MG.heartbeatTicker = nil
