bb95502a0f6a8a7d3bbfef62759ef3758e68ba0b
Riflex91 <hansijuergenlul@gmail.com>
2026-09-21 18:17:49 +0200

    V5: export bank deposit shadow admission

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -153,6 +153,7 @@ export * from "./merchant/bank-produktions-modul-vertrag.js";
 export * from "./merchant/bank-produktions-faehigkeits-vertrag.js";
 export * from "./merchant/bank-deposit-einmal-authority.js";
 export * from "./merchant/bank-deposit-admission-gate.js";
+export * from "./merchant/bank-deposit-shadow-admission.js";
 export * from "./merchant/verkaufs-sicherheit.js";
 export * from "./merchant/markt-historie.js";
 export * from "./merchant/mluck-service.js";
