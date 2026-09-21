6611e7dee3b096d19e2f35c3c118865cbd92a4c5
Riflex91 <hansijuergenlul@gmail.com>
2026-09-21 12:40:46 +0200

    V5: export gold transfer settlement verifier

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -150,3 +150,4 @@ export * from "./merchant/bank-autonomie.js";
 export * from "./merchant/verkaufs-sicherheit.js";
 export * from "./merchant/markt-historie.js";
 export * from "./merchant/mluck-service.js";
+export * from "./merchant/gold-transfer-settlement.js";
