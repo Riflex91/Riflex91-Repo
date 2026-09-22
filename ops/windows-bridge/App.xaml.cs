ca3166d0f29bd0805a6c36c27ff06299588a4f6f
Riflex91 <hansijuergenlul@gmail.com>
2026-09-22 22:51:18 +0200

    Add Windows Bridge automatic self-update

--- ops/windows-bridge/App.xaml.cs
+++ ops/windows-bridge/App.xaml.cs
@@ -4,4 +4,21 @@ namespace AioBotWindowsBridge;
 
 public partial class App : Application
 {
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        if (WindowsBridgeUpdateBootstrap.IsApplyUpdateMode(e.Args))
+        {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            var exitCode = await WindowsBridgeUpdateBootstrap.ApplyAsync(e.Args);
+            Shutdown(exitCode);
+            return;
+        }
+
+        ShutdownMode = ShutdownMode.OnMainWindowClose;
+        var window = new MainWindow();
+        MainWindow = window;
+        window.Show();
+    }
 }
