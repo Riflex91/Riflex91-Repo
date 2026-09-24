2b998a9ecb8aa51561c3599153a75f3571e4f17d
Riflex91 <hansijuergenlul@gmail.com>
2026-09-24 06:33:48 +0200

    PR20.7: export weapon offhand preparation foundation

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -186,3 +186,4 @@ export * from "./merchant/dienst-stabilitaet.js";
 export * from "./equipment/pr20-7-gear-swap-vorbereitung.js";
 export * from "./equipment/pr20-7-gear-swap-one-shot-authority.js";
 export * from "./equipment/pr20-7-gear-swap-durable-intent-reconcile.js";
+export * from "./equipment/pr20-7-weapon-offhand-vorbereitung.js";
