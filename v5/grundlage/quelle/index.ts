f85630c9222e39f62e765c07aec270c789e05901
Riflex91 <hansijuergenlul@gmail.com>
2026-09-25 09:51:49 +0200

    PR20.9: export craft durable shadow foundation

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -112,6 +112,7 @@ export * from "./produktion/production-intent.js";
 export * from "./produktion/production-planer.js";
 export * from "./produktion/production-controller.js";
 export * from "./produktion/pr20-9-craft-read-only-preflight.js";
+export * from "./produktion/pr20-9-craft-durable-shadow.js";
 export * from "./merchant/supply-policy.js";
 export * from "./merchant/logistik-workflow.js";
 export * from "./merchant/logistik-planer.js";
