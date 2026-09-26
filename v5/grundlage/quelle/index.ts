f262da972470ca206a1f98df7ce822280e3fc64b
Riflex91 <hansijuergenlul@gmail.com>
2026-09-26 10:22:14 +0200

    PR21: fix checkpoint admission export newline

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -198,7 +198,8 @@ export * from "./equipment/pr20-7-weapon-offhand-acquisition-shadow.js";
 export * from "./equipment/pr20-7-weapon-offhand-acquisition-purchase-preparation.js";
 
 export * from "./merchant/pr21-merchant-integration-readiness.js";
-export * from "./merchant/pr21-merchant-integration-live-preflight.js";\nexport * from "./merchant/pr21-merchant-checkpoint-admission.js";
+export * from "./merchant/pr21-merchant-integration-live-preflight.js";
+export * from "./merchant/pr21-merchant-checkpoint-admission.js";
 export * from "./koordination/pr22-coordination-shadow-admission.js";
 export * from "./koordination/production-material-acquisition.js";
 export * from "./koordination/production-material-handoff.js";
