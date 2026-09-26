191255c1ae10c9a40c3d3999c54a2dfbffe954c6
Riflex91 <hansijuergenlul@gmail.com>
2026-09-26 09:27:14 +0200

    Export PR21 Merchant live preflight

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -198,6 +198,7 @@ export * from "./equipment/pr20-7-weapon-offhand-acquisition-shadow.js";
 export * from "./equipment/pr20-7-weapon-offhand-acquisition-purchase-preparation.js";
 
 export * from "./merchant/pr21-merchant-integration-readiness.js";
+export * from "./merchant/pr21-merchant-integration-live-preflight.js";
 export * from "./koordination/pr22-coordination-shadow-admission.js";
 export * from "./koordination/production-material-acquisition.js";
 export * from "./koordination/production-material-handoff.js";
