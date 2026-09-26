661da26df2d9d7cb416a0c06ef832bc1cbe2e108
Riflex91 <hansijuergenlul@gmail.com>
2026-09-26 18:19:42 +0200

    PR21: export live result freeze adapter

--- v5/grundlage/quelle/index.ts
+++ v5/grundlage/quelle/index.ts
@@ -203,6 +203,7 @@ export * from "./merchant/pr21-merchant-checkpoint-admission.js";
 export * from "./merchant/pr21-merchant-observer-handoff.js";
 export * from "./merchant/pr21-merchant-sample-collector.js";
 export * from "./merchant/pr21-merchant-freeze-evaluation-handoff.js";
+export * from "./merchant/pr21-merchant-live-result-freeze-adapter.js";
 export * from "./merchant/pr21-merchant-ratification-record-boundary.js";
 export * from "./merchant/pr21-merchant-gate-proposal-boundary.js";
 export * from "./merchant/pr21-merchant-default-off-apply-boundary.js";
